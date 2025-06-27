8bfc6d0c06f44c2bd7bbc746d8d3d6f713501cb2
Tanmay Mishu <tanmaymishu@gmail.com>
2025-06-27 16:27:54 +0600

    Synchronize source, category, author

--- app/Actions/SyncArticlesAction.php
+++ app/Actions/SyncArticlesAction.php
@@ -3,6 +3,9 @@
 namespace App\Actions;
 
 use App\Models\Article;
+use App\Models\Author;
+use App\Models\Category;
+use App\Models\Source;
 
 class SyncArticlesAction
 {
@@ -12,6 +15,24 @@ class SyncArticlesAction
             $attributes,
             ['web_url'],
             ['title', 'category', 'source', 'description', 'content', 'author', 'web_url', 'featured_image_url'],
+        );
+
+        Source::query()->upsert(
+            collect($attributes)->pluck('source')->map(fn($source) => ['name' => $source])->toArray(),
+            ['name'],
+            ['name'],
+        );
+
+        Author::query()->upsert(
+            collect($attributes)->pluck('author')->map(fn($author) => ['name' => $author])->toArray(),
+            ['name'],
+            ['name'],
+        );
+
+        Category::query()->upsert(
+            collect($attributes)->pluck('category')->map(fn($category) => ['name' => $category])->toArray(),
+            ['name'],
+            ['name'],
         );
     }
 }
--- database/migrations/2025_06_26_100025_create_articles_table.php
+++ database/migrations/2025_06_26_100025_create_articles_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('category');
-            $table->string('source');
-            $table->string('title');
+            $table->string('category')->default('Uncategorized')->index();
+            $table->string('source')->default('Unknown')->index();
+            $table->string('title')->index();
             $table->text('description');
             $table->text('content');
             $table->text('web_url')->unique(); // text, because some sources have really long URLs
             $table->text('featured_image_url'); // text, because some sources have really long URLs
-            $table->string('author')->default("News Desk");
+            $table->string('author')->default('Staff Reporter')->index();
             $table->dateTime('published_at');
 
             $table->timestamps();
